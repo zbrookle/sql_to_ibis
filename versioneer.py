5db10788f5ae2a8082de0c0aa630272acc34cc0b
zbrookle <zachb1996@yahoo.com>
2020-06-02 15:40:39 -0400

    ENH: Don't sort versioneer isort

--- versioneer.py
+++ versioneer.py
@@ -273,6 +273,7 @@ Specifically, both are released under the Creative Commons "Public Domain
 Dedication" license (CC0-1.0), as described in
 https://creativecommons.org/publicdomain/zero/1.0/ .
 
+isort:skip_file
 """
 
 from __future__ import print_function
