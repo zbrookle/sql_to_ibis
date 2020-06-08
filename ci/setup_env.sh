670bd9b6f5b2ccd211f87c8eab79bcf09dfaebde
zbrookle <zachb1996@yahoo.com>
2020-06-08 15:15:50 -0400

    ENH: Revert to before pandas added to dependencies

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+# edit the locale file if needed
+if [[ "$(uname)" == "Linux" && -n "$LC_ALL" ]]; then
+    echo "Adding locale to the first line of pandas/__init__.py"
+    rm -f pandas/__init__.pyc
+    SEDC="3iimport locale\nlocale.setlocale(locale.LC_ALL, '$LC_ALL')\n"
+    sed -i "$SEDC" pandas/__init__.py
+
+    echo "[head -4 pandas/__init__.py]"
+    head -4 pandas/__init__.py
+    echo
+fi
+
 MINICONDA_DIR="$HOME/miniconda3"
 
 
@@ -44,7 +56,30 @@ conda update -n base conda
 echo "conda info -a"
 conda info -a
 
-echo "conda deactivate"
+echo
+echo "set the compiler cache to work"
+if [ -z "$NOCACHE" ] && [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    echo "Using ccache"
+    export PATH=/usr/lib/ccache:/usr/lib64/ccache:$PATH
+    GCC=$(which gcc)
+    echo "gcc: $GCC"
+    CCACHE=$(which ccache)
+    echo "ccache: $CCACHE"
+    export CC='ccache gcc'
+elif [ -z "$NOCACHE" ] && [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    echo "Install ccache"
+    brew install ccache > /dev/null 2>&1
+    echo "Using ccache"
+    export PATH=/usr/local/opt/ccache/libexec:$PATH
+    gcc=$(which gcc)
+    echo "gcc: $gcc"
+    CCACHE=$(which ccache)
+    echo "ccache: $CCACHE"
+else
+    echo "Not using ccache"
+fi
+
+echo "source deactivate"
 source deactivate
 
 echo "conda list (root environment)"
@@ -76,5 +111,15 @@ conda remove qt -y --force || true
 echo
 echo "conda list"
 conda list
+
+# Install DB for Linux
+
+if [[ -n ${SQL:0} ]]; then
+  echo "installing dbs"
+  mysql -e 'create database pandas_nosetest;'
+  psql -c 'create database pandas_nosetest;' -U postgres
+else
+   echo "not using dbs on non-linux Travis builds or Azure Pipelines"
+fi
 
 echo "done"
