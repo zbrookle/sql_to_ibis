1417bcb8303c762bf9c10a9674b031bf52887d6c
zbrookle <zachb1996@yahoo.com>
2020-08-29 17:53:46 -0400

    CLN: Remove unused class

--- sql_to_ibis/sql/sql_clause_objects.py
+++ sql_to_ibis/sql/sql_clause_objects.py
@@ -4,23 +4,6 @@ from typing import Optional, Union
 from sql_to_ibis.sql.sql_value_objects import Column, JoinBase, Subquery, Table, Value
 
 
-class RowRangeClause:
-    _row = "ROW"
-    _range = "RANGE"
-    _clause_types = [_row, _range]
-
-    def __init__(
-        self,
-        clause_type: str,
-        preceding: Optional[Union[int, tuple]],
-        following: Optional[Union[int, tuple]],
-    ):
-        if clause_type not in self._clause_types:
-            raise Exception(f"Type must be one of {self._clause_types}")
-        self.preceding = preceding
-        self.following = following
-
-
 @dataclass
 class LimitExpression:
     limit: int
