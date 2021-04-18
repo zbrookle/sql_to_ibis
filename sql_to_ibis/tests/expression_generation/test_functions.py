39d0e8a78ed4514b3b91a9bffd88c9d6c39c6004
zbrookle <zachb1996@yahoo.com>
2021-04-18 12:32:52 -0400

    ENH: Add test for coalesce

--- sql_to_ibis/tests/expression_generation/test_functions.py
+++ sql_to_ibis/tests/expression_generation/test_functions.py
@@ -6,8 +6,9 @@ from sql_to_ibis.tests.utils import assert_ibis_equal_show_diff, assert_state_no
 
 @assert_state_not_change
 def test_coalesce(forest_fires):
-    my_table = query("select coalesce(day, month, 2) as my_number from forest_fires")
-    ibis_table = ibis.coalesce(9, forest_fires.day, forest_fires.month).name(
-        "my_number"
-    )
+    my_table = query("select coalesce(day, month) as my_number from forest_fires")
+    print(my_table.columns)
+    ibis_table = forest_fires[
+        ibis.coalesce(forest_fires.day, forest_fires.month).name("my_number")
+    ]
     assert_ibis_equal_show_diff(ibis_table, my_table)
